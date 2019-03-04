2941b9fd20b6e261a80f3f80f7b2af4022c88751
daviddahlb <davidsdahlberg@gmail.com>
2019-03-04 02:31:04 -0800

    Required stories complete -Parse implemented -User can log in -User can create a post with a camera picture

--- app/src/main/java/com/dsd3/sofoto/ParseApplication.java
+++ app/src/main/java/com/dsd3/sofoto/ParseApplication.java
@@ -3,6 +3,7 @@ package com.dsd3.sofoto;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -22,6 +23,7 @@ public class ParseApplication extends Application {
         httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         builder.networkInterceptors().add(httpLoggingInterceptor);
 
+        ParseObject.registerSubclass(Post.class);
         // set applicationId, and server server based on the values in the Heroku settings.
         // clientKey is not needed unless explicitly configured
         // any network interceptors must be added with the Configuration Builder given this syntax
